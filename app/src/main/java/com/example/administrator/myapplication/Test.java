b08782b5024705badbd3fb11944811b42c29ad50
puzuhui <2753015604@qq.com>
2017-11-23 10:49:08 +0800

    Initial commit

---
+++ app/src/main/java/com/example/administrator/myapplication/Test.java
@@ -0,0 +1,24 @@
+package com.example.administrator.myapplication;
+
+import android.util.Log;
+
+/**
+ * Created by Administrator on 2017/11/20 0020.
+ */
+
+public class Test {
+
+    public String brid;
+
+    public Test(String brid){
+        brid = this.brid;
+    }
+
+    public String getBrid(){
+        return brid;
+    }
+
+    public void setBrid(String brid){
+        this.brid = brid;
+    }
+}
